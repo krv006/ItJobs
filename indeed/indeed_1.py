3b6fef69c9b170ec711fa9ddbcd57a1864889c4b
ibrohimpardaboyev <ibrohim.pardaboyev1@gmail.com>
2025-07-15 13:08:37 +0500

    Update indeed_1.py

--- indeed/indeed_1.py
+++ indeed/indeed_1.py
@@ -14,9 +14,6 @@ def create_driver(headless=False):
         options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("start-maximized")
-    options.add_argument(
-        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
     driver = uc.Chrome(options=options, version_main=137)
     return driver
 
