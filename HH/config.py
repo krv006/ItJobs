1fad505a95ecc77a996a6fb5ea3c75df0aefc6de
krv006 <rvkamronbek@gmail.com>
2025-07-19 11:34:29 +0500

    sql indeed

--- HH/config.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-
-driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 10)
-ec = EC
